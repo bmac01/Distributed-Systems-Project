998f2a5ead4565fec805a40887acd62d326ac39d
bmac01 <92854061+bmac01@users.noreply.github.com>
2024-01-03 18:35:25 +0000

    add server file - light toggle

---
+++ smart-home-app/server/server.js
@@ -0,0 +1,28 @@
+var grpc = require('@grpc/grpc-js');
+var protoLoader = require('@grpc/proto-loader');
+
+var packageDef = protoLoader.loadSync('smarthome.proto', {});
+var grpcObject = grpc.loadPackageDefinition(packageDef);
+var smarthomecontrolPackage = grpcObject.smarthomecontrol;
+
+var server = new grpc.Server();
+let lightStatus = false; // needs to be updated so that it is random?? 
+
+server.addService(smarthomecontrolPackage.LightService.service, {
+    ToggleLight: (call, callback) => {
+        lightStatus = call.request.status;
+        callback(null, {
+            result: lightStatus,
+            message: `Light is now ${lightStatus ? 'ON' : 'OFF'}`
+        });
+    }
+});
+
+server.bindAsync('0.0.0.0:4000', grpc.ServerCredentials.createInsecure(), (error, port) => {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    server.start();
+    console.log(`gRPC server running on port ${port}`);
+});
